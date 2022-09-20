20b349fe82140ae9f9225d82780a273c515c165c
sitinabila30 <s.nabila3006@gmail.com>
2022-09-21 00:24:44 +0700

    weblanjut

---
+++ 2057051006_PrakWeb4/app/Views/mahasiswa/list.php
@@ -0,0 +1,38 @@
+<div class="p-4">
+
+    <a href="/create" type="button" class="btn btn-primary">Tambah Data</a>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">NPM</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">Created_At</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 1; ?>
+            <?php foreach ($mahasiswa as $mhs) : ?>
+                <tr>
+                    <th scope="row"><?= $no; ?></th>
+                    <td><?= $mhs['npm']; ?></td>
+                    <td><?= $mhs['nama']; ?></td>
+                    <td><?= $mhs['alamat']; ?></td>
+                    <td><?= $mhs['created_at']; ?></td>
+                    <td>
+                        <div class="d-flex">
+                            <a class="btn btn-warning mr-3" href="/edit/<?= $mhs['id'] ?>">Edit</a>
+                            <form action="/delete/<?= $mhs['id'] ?>" method="post">
+                                <input type="hidden" name="_method" value="DELETE">
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+            <?php $no++;
+            endforeach; ?>
+        </tbody>
+    </table>
+</div>
